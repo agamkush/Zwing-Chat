09fd8e3fb9046be224fec53680f4082d64f6cfd9
agamkush <agam.k@gsl.in>
2025-05-11 01:51:49 +0530

    deployment

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -97,8 +97,5 @@ export const useAuthStore = create((set, get) => ({
     socket.on("getOnlineUsers", (userIds) => {
       set({ onlineUsers: userIds });
     });
-  },
-  disconnectSocket: () => {
-    if (get().socket?.connected) get().socket.disconnect();
   },
 }));
